f8a9a70df6877322c834fd1acd17dcf06ef1a575
mhafizhurrahman <hafizhafizdb@gmail.com>
2023-10-31 16:01:37 +0800

    first commit

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Welcome extends CI_Controller
+{
+	public function about()
+	{
+		// fungsi untuk me-load view about.php
+		$this->load->view('about');
+	}
+	public function profile()
+	{
+		// fungsi untuk me-load view contact.php
+		$this->load->view('profile');
+	}
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/userguide3/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
---
+++ application/views/profile.php
@@ -0,0 +1,2 @@
+<h1>profile Us</h1>
+<p>ini halaman profile</p>
